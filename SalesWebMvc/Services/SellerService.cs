cae0b4dd5dd7515391ef37409b66b823181405e7
danilo <danilodramos@yahoo.com.br>
2023-01-25 16:36:25 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -32,9 +32,16 @@ namespace SalesWebMvc.Services
         }
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Nao posso deletar  vendedor pois ele/ela já tem vendas");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
